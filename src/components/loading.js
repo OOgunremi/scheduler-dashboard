a628e62648b5abd1f77c435bae350ed25c4566c1
Olalekan Isaac Ogunremi <OOgunremi@yahoo.com>
2022-04-28 20:53:11 -0600

    develop panel and loading components

---
+++ src/components/loading.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import classNames from "classnames";
+
+class Loading extends Component {
+
+  render () {
+    return (
+      <section className="loading">Loading!</section>
+    )
+  }
+}
+export default Loading;
